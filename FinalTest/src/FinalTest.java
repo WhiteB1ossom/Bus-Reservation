138399bbff06ae48d13031987f17b4851698c457
Blossom <blossom5831@naver.com>
2024-11-21 21:03:52 +0900

    #24.11.21 셔틀버스 예약 시스템 개발 시작

---
+++ FinalTest/src/FinalTest.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+
+public class FinalTest extends JFrame {
+	
+	FinalTest() {
+		this.setSize(720, 520);
+		this.setTitle("셔틀버스 자리 예약 시스템");
+		
+		
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+	
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new FinalTest();
+	}
+
+}
